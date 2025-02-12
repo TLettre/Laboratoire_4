5020d09d778c305e263bb24f9dc73675254fad3e
Tristan Lettre <206296182@cegepsherbrooke.qc.ca>
2025-02-12 13:29:23 -0500

    analyse.py

---
+++ analyse.py
@@ -0,0 +1,18 @@
+"""
+Il y a plusieur chose a améliorer ou a ajouter au code. 
+seulement le squelette  du code est complèté.
+pour le moment la seul chose fontionnel c'est le chois du pokemon et le chois des attaque. 
+
+il faudra:
+
+ajouter du du texte pour l'utilisateur 
+
+terminer la configuration des attaque 
+
+créé un adversaire. avec un système de tour par tour.
+
+
+c'est Étape ne seront probablement pas terminer au cours d'une seul Période. 
+tout les modification seront dans modifications.py
+
+"""
